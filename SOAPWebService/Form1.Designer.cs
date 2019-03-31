8a654ff365aeb6259e338b87cc33ce662055fc45
daniel <guede-91@hotmail.com>
2019-03-31 12:05:00 +0100

    uploading project to gitHub

---
+++ SOAPWebService/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SOAPWebService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.labelResult2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "TO";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "FROM";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(123, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(248, 42);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(66, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 20);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(136, 84);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(53, 13);
+            this.labelResult.TabIndex = 12;
+            this.labelResult.Text = "RESULT:";
+            // 
+            // labelResult2
+            // 
+            this.labelResult2.AutoSize = true;
+            this.labelResult2.Location = new System.Drawing.Point(188, 106);
+            this.labelResult2.Name = "labelResult2";
+            this.labelResult2.Size = new System.Drawing.Size(10, 13);
+            this.labelResult2.TabIndex = 13;
+            this.labelResult2.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 173);
+            this.Controls.Add(this.labelResult2);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label labelResult2;
+    }
+}
+
---
+++ SOAPWebService/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using SOAPWebService.net.webservicex.www;
+
+namespace SOAPWebService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public CurrencyConvertor currencyconverter = new CurrencyConvertor();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Currency currencyFrom = (Currency)comboBox1.SelectedItem;
+            Currency currencyTo   = (Currency)comboBox2.SelectedItem;
+
+            double value = currencyconverter.ConversionRate(currencyFrom,currencyTo);
+
+            labelResult.Text =  "The difference with the two different coins is:";
+            labelResult2.Text = value + "   ";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = Enum.GetValues(typeof(Currency));
+            comboBox1.SelectedItem = Currency.USD;
+
+            comboBox2.DataSource = Enum.GetValues(typeof(Currency));
+            comboBox2.SelectedItem = Currency.EUR;
+        }
+    }
+}
